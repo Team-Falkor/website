e0202add80c957018d5df0ba22ea1c5f28b351c0
prostarz <tommyspam2000@gmail.com>
2024-09-25 00:10:21 +0100

    fix(ui): ensure uniform height for feature items without setting explicit height
    - Applied Flexbox to the `FeatureItem` component to make all feature items the same height.
    - Adjusted the layout to allow for flexible content inside each feature item.
    - Ensured responsiveness and consistent alignment across different screen sizes.

--- src/components/features/items/item.tsx
+++ src/components/features/items/item.tsx
@@ -4,7 +4,6 @@ type FeatureItemProps = {
   title: string;
   children: string;
   icon: JSX.Element;
-
   isAvailable?: boolean;
   link?: string;
 };
@@ -17,28 +16,27 @@ const FeatureItem: FC<FeatureItemProps> = ({
   link,
 }) => {
   return (
-    <div className="relative flex gap-4 p-6 hover:bg-secondary-foreground/5 rounded-lg">
-      <div className="text-purple-400">{icon}</div>
-      <div className="flex flex-col gap-2">
-        <div className="inline text-lg font-semibold text-white">{title}</div>
-        <dd className="h-24 text-muted-foreground">{children}</dd>
-
-        <div className="flex items-center gap-4 mt-2.5 sm:mt-0">
-          {isAvailable ? (
-            <a
-              href={link}
-              target={"_blank"}
-              rel="noopener noreferrer"
-              className="inline-flex items-center px-2 py-1 mt-2 text-xs font-medium text-purple-400 rounded-md bg-purple-500/10 ring-purple-500/20 ring-1 ring-inset"
-            >
-              Available
-            </a>
-          ) : (
-            <span className="inline-flex items-center px-2 py-1 mt-2 text-xs font-medium text-yellow-400 rounded-md bg-yellow-500/10 ring-yellow-500/20 ring-1 ring-inset ">
-              Coming Soon
-            </span>
-          )}
-        </div>
+    <div className="relative flex flex-col h-full p-6 hover:bg-secondary-foreground/5 rounded-lg">
+      <div className="flex items-start gap-4">
+        <div className="text-purple-400">{icon}</div>
+        <div className="text-lg font-semibold text-white">{title}</div>
+      </div>
+      <dd className="flex-grow mt-2 text-muted-foreground">{children}</dd>
+      <div className="flex items-center gap-4 mt-4">
+        {isAvailable ? (
+          <a
+            href={link}
+            target={"_blank"}
+            rel="noopener noreferrer"
+            className="inline-flex items-center px-2 py-1 text-xs font-medium text-purple-400 rounded-md bg-purple-500/10 ring-purple-500/20 ring-1 ring-inset"
+          >
+            Available
+          </a>
+        ) : (
+          <span className="inline-flex items-center px-2 py-1 text-xs font-medium text-yellow-400 rounded-md bg-yellow-500/10 ring-yellow-500/20 ring-1 ring-inset">
+            Coming Soon
+          </span>
+        )}
       </div>
     </div>
   );
