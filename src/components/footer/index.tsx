a49860ed20e2909abf73fed6db1364d7a4e922d1
prostarz <tommyspam2000@gmail.com>
2024-09-26 01:59:06 +0100

    fix: some colour issues

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,13 +4,15 @@ import { FaDiscord, FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="mt-5 flex flex-col items-center justify-between gap-5 bg-muted/40 p-5 px-14 text-center sm:flex-row sm:gap-1 sm:text-left">
+    <div className="mt-5 flex flex-col items-center justify-between gap-5 bg-muted/30 p-5 px-14 text-center sm:flex-row sm:gap-1 sm:text-left">
       <div className="flex flex-col gap-1">
         <p className="text-sm font-medium">
           © {new Date().getFullYear()} Falkor
         </p>
 
-        <p className="text-sm font-medium">Made with ❤️ by Team Falkor</p>
+        <p className="text-sm text-muted-foreground">
+          Made with ❤️ by Team Falkor
+        </p>
       </div>
 
       <div className="flex flex-row gap-3">
