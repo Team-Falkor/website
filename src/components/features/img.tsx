3f33f550276a6b2316159e28462b011c4d9b4e07
prostarz <tommyspam2000@gmail.com>
2024-10-03 03:48:56 +0100

    feat(ui): feature image is now a carousel

--- src/components/features/img.tsx
+++ src/components/features/img.tsx
@@ -1,11 +1,23 @@
-const FeaturesImg = () => {
+import { cn } from "@/utils/utils.ts";
+import { HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt: string;
+}
+
+const FeaturesImg = ({ src, alt, className, ...props }: Props) => {
   return (
     <div className="relative pt-16 overflow-hidden">
       <div className="relative px-6 mx-auto overflow-hidden max-w-7xl lg:px-8">
         <img
-          src="/app-info.webp"
-          alt="App Info Screen Screenshot"
-          className="rounded-xl shadow-2xl ring-1 relative z-10 ring-white/10 object-contain"
+          src={src}
+          alt={alt}
+          className={cn([
+            "rounded-xl shadow-2xl ring-1 relative z-10 ring-white/10 object-contain",
+            className,
+          ])}
+          {...props}
         />
         <div className="absolute bottom-0 z-20 pointer-events-none opacity-65 bg-gradient-to-t from-background to-transparent size-full" />
       </div>
--- src/components/features/index.tsx
+++ src/components/features/index.tsx
@@ -1,28 +1,77 @@
 import FeaturesImg from "@/components/features/img";
 import FeaturesItems from "@/components/features/items";
+import Autoplay from "embla-carousel-autoplay";
+import { useRef } from "react";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 
-const Features = () => (
-  <div className="mt-24 sm:mt-40">
-    <div className="px-6 mx-auto max-w-7xl lg:px-8">
-      <div className="max-w-2xl mx-auto sm:text-center">
-        <h2 className="text-base font-semibold leading-7 text-purple-400">
-          Everything you need
-        </h2>
-        <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-          Gaming, Redefined!
-        </p>
-        <p className="mt-6 text-lg leading-8 text-gray-300">
-          Dive into a universe where convenience meets innovation, with Falkor
-          providing community plugins, release calendars, and comprehensive game
-          management in one seamless platform.
-        </p>
+const images: {
+  src: string;
+  alt: string;
+}[] = [
+  {
+    src: "/app-info.webp",
+    alt: "App Info Screenshot",
+  },
+  {
+    src: "/app-info-2.webp",
+    alt: "App Info Screenshot",
+  },
+  {
+    src: "/app-info-3.webp",
+    alt: "App Info Screenshot",
+  },
+  {
+    src: "/app-library.webp",
+    alt: "App Library Screenshot",
+  },
+];
+
+const Features = () => {
+  const autoplay = useRef(
+    Autoplay({
+      delay: 5000,
+      stopOnInteraction: true,
+      stopOnMouseEnter: true,
+      stopOnFocusIn: true,
+    })
+  );
+
+  return (
+    <div className="mt-24 sm:mt-40">
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto sm:text-center">
+          <h2 className="text-base font-semibold leading-7 text-purple-400">
+            Everything you need
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Gaming, Redefined!
+          </p>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Dive into a universe where convenience meets innovation, with Falkor
+            providing community plugins, release calendars, and comprehensive
+            game management in one seamless platform.
+          </p>
+        </div>
       </div>
+
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        plugins={[autoplay.current]}
+      >
+        <CarouselContent>
+          {images.map(({ src, alt }) => (
+            <CarouselItem>
+              <FeaturesImg key={src} src={src} alt={alt} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      <FeaturesItems />
     </div>
-
-    <FeaturesImg />
-
-    <FeaturesItems />
-  </div>
-);
+  );
+};
 
 export default Features;
