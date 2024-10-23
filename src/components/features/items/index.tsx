fc2a2fa8c163768c99b2a130b2fa6c82e0cfeb3e
prostarz <tommyspam2000@gmail.com>
2024-10-23 13:47:01 +0100

    refactor: turn features into an map

--- src/components/features/items/index.tsx
+++ src/components/features/items/index.tsx
@@ -1,63 +1,29 @@
 import FeatureItem from "@/components/features/items/item";
-import {
-  CalendarDays,
-  Code2,
-  HardDrive,
-  Puzzle,
-  Rocket,
-  Users,
-} from "lucide-react";
+import { useMemo } from "react";
+import { featureItems } from "./items";
 
 const FeaturesItems = () => {
+  const items = useMemo(() => {
+    return featureItems.sort((a, b) => {
+      if (a.isAvailable && !b.isAvailable) return -1;
+      if (!a.isAvailable && b.isAvailable) return 1;
+      return 0;
+    });
+  }, []);
+
   return (
     <div className="px-6 mx-auto mt-16 max-w-7xl sm:mt-24 lg:px-8">
       <dl className="grid max-w-2xl grid-cols-1 mx-auto text-base leading-7 text-gray-300 gap-x-6 gap-y-10 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-        <FeatureItem icon={<Users />} title="Plugins by Community." isAvailable>
-          Enhance your gaming experience with plugins created by our community,
-          tailored to your preferences.
-        </FeatureItem>
-
-        <FeatureItem icon={<CalendarDays />} title="Calendar">
-          Stay ahead with our release calendar, marking the debut of new games
-          so you never miss out on the latest titles.
-        </FeatureItem>
-
-        <FeatureItem
-          icon={<Code2 />}
-          title="Open Source."
-          isAvailable
-          link={"/github/app"}
-        >
-          Dive into a platform that thrives on collaboration and innovation,
-          powered by an open-source ethos.
-        </FeatureItem>
-
-        <FeatureItem
-          icon={<Puzzle />}
-          title="Game Library Management."
-          isAvailable={false}
-        >
-          Organize and manage your installed games effortlessly with our
-          intuitive library system.
-        </FeatureItem>
-
-        <FeatureItem
-          icon={<Rocket />}
-          title="Launcher Integration."
-          isAvailable={false}
-        >
-          Launch your games directly from the app, streamlining your playtime
-          with our integrated launcher.
-        </FeatureItem>
-
-        <FeatureItem
-          icon={<HardDrive />}
-          title="Inbuilt Torrent Client."
-          isAvailable={false}
-        >
-          Experience seamless file sharing with our integrated torrent client,
-          designed for fast and secure transfers.
-        </FeatureItem>
+        {items.map((item) => (
+          <FeatureItem
+            icon={item.icon}
+            title={item.title}
+            isAvailable={item.isAvailable ?? false}
+            link={item.link}
+          >
+            {item.description}
+          </FeatureItem>
+        ))}
       </dl>
     </div>
   );
