61b49ecd1c23fad29ceb03f03cb5e26bf201ef60
prostarz <tommyspam2000@gmail.com>
2024-10-23 05:39:49 +0100

    update: homepage

--- src/components/hero/badges.tsx
+++ src/components/hero/badges.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight } from 'lucide-react';
+import { ChevronRight } from "lucide-react";
 
 const HeroBadges = () => {
   return (
@@ -6,9 +6,9 @@ const HeroBadges = () => {
       <span className="px-3 py-1 text-sm font-semibold leading-6 text-purple-400 rounded-full bg-purple-700/25 ring-1 ring-inset ring-purple-700/40">
         App In Progress
       </span>
-      <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
+      <span className="inline-flex items-center gap-1.5 text-sm font-medium leading-6 text-gray-300">
         Coming soon
-        <ChevronRight />
+        <ChevronRight strokeWidth={3} />
       </span>
     </div>
   );
