59a252664066769f2e127a88783692a2332895bc
prostarz <tommyspam2000@gmail.com>
2024-10-30 04:46:29 +0000

    update: footer links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { constants } from "@/utils";
 import { FaDiscord, FaGithub } from "react-icons/fa6";
+import { SiKofi } from "react-icons/si";
 
 const Footer = () => {
+  const { ko_fi_url } = constants;
   return (
     <div className="mt-5 flex flex-col items-center justify-between gap-5 bg-muted/30 p-5 px-14 text-center sm:flex-row sm:gap-1 sm:text-left">
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1.5">
         <p className="text-sm font-medium">
           © {new Date().getFullYear()} Falkor
         </p>
@@ -17,7 +20,7 @@ const Footer = () => {
 
       <div className="flex flex-row gap-3">
         <a
-          className="size-5 transition-all hover:opacity-50"
+          className="size-5 transition-all hover:text-purple-400"
           target="_blank"
           href={"/github/website"}
         >
@@ -25,11 +28,19 @@ const Footer = () => {
         </a>
 
         <a
-          className="size-5 transition-all hover:opacity-50"
+          className="size-5 transition-all  hover:text-purple-400"
           target="_blank"
           href={"/discord"}
         >
           <FaDiscord className="size-full" />
+        </a>
+
+        <a
+          className="size-5 transition-all hover:text-purple-400"
+          target="_blank"
+          href={ko_fi_url}
+        >
+          <SiKofi className="size-full" />
         </a>
       </div>
     </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,7 @@ export const constants = {
   discord_invite_link_flww: "https://discord.gg/V75hgQA9ue",
   github_website_url: "https://github.com/team-falkor/website",
   github_app_url: "https://github.com/team-falkor/app",
+  ko_fi_url: "https://ko-fi.com/prostarz",
   github_repos: ["app", "website", "sdk", "discord-bot", "plugin-template"],
   app_version: "V0.0.4",
   banner_message: "Test Build V0.0.4 - NOW Available for Download!!",
