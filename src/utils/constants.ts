d13e1d92c260f39dc5c87218fa5ad2df8ddc32f3
prostarz <tommyspam2000@gmail.com>
2025-01-15 01:12:05 +0000

    update version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,9 @@
 export const constants = {
   discord_invite_link: "https://discord.gg/5cxfH6ak9h",
-  discord_invite_link_flww: "https://discord.gg/V75hgQA9ue",
   github_website_url: "https://github.com/team-falkor/website",
   github_app_url: "https://github.com/team-falkor/app",
   ko_fi_url: "https://ko-fi.com/prostarz",
   github_repos: ["app", "website", "discord-bot"],
-  app_version: "v0.0.92",
-  banner_message: "Test Build V0.0.92",
+  app_version: "v0.1.0",
+  banner_message: "Test Build V0.0.1 W/ Achievements support",
 };
